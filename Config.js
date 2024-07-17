87961d3a36208d0b3360821a23fdf0f1f1e0ff57
cordyboy857 <paulosaruwemse@gmail.com>
2024-07-17 01:47:28 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,18 +3,18 @@ const chalk = require('chalk');
 require('dotenv').config();
 
 //contact details
-global.ownernumber = [ process.env.OWNER_NUMBER || "919931122319"],
-global.ownername = process.env.OWNER_NAME || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.ownernumber = [ process.env.OWNER_NUMBER || "2349020205926"],
+global.ownername = process.env.OWNER_NAME || "çørdyBōy",
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.location = "Ningeria"
 
 global.botname = process.env.BOTNAME || "𝐌𝐀𝐑𝐈𝐀-𝐌𝐃",//name of the bot
 
 //sticker details
-global.stickername = process.env.STICKER || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.stickername = process.env.STICKER || "cordyboy",
 global.packname = 'Sticker By'
-global.author = process.env.AUTHOR || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.author = process.env.AUTHOR || "cordyboy",
 //console view/theme
 global.themeemoji = '🧩'
 global.wm = "Ayush botz inc."
@@ -28,12 +28,12 @@ global.prefa = process.env.PREFIX || ".",
 //false=disable and true=enable
 global.welcome = process.env.WELCOME || "false", //auto welcome
 global.autoRecording = false //auto recording
-global.autoTyping = false //auto typing
+global.autoTyping = true //auto typing
 global.autorecordtype = false //auto typing + recording
 global.autoread = false //auto read messages
 global.autobio = false //auto update bio
 global.anti212 = true //auto block +212
-global.autoread_status = false //auto view status/story
+global.autoread_status = true //auto view status/story
 
 
 
@@ -51,11 +51,11 @@ global.mess = {
 }
 
 module.exports = {
-ownernumber: global.ownernumber,
-ownername: global.ownername,
-sessionId: process.env.id,
+ownernumber: global.ownernumber,"2349020205926"
+ownername: global.ownername,"Cordyboy"
+sessionId:"_M_A_R_I_A_SGFpdGlBbWFuZGEK process.env.id",
 AUTO_BLOCK : process.env.PM_BLOCKER,
-STATUS_SAVER : "true",
+STATUS_SAVER : "false",
 ANTI_BOT:"true",
 }
 
